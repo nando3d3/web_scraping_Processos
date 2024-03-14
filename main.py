3e5fc5f633321ac4ed0daa4ce2f177d721dec432
pedroblome <joaopedroblome78@gmail.com>
2024-03-14 17:27:14 -0300

    mudar framework do flask para fastApi

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-import Cors
-import os
-import views
+from fastapi import FastAPI
 
-app=Cors.App
+from fastapi import FastAPI, Request
+import json
+from ProcessSearcher import ProcessSearcher
 
-if __name__=="__main__":
-    app.run(debug= True)
+
+app = FastAPI()
+
+
+@app.post("/")
+async def search_process(request: Request):
+
+    data_request = (
+        await request.json()
+    )  # Obter os dados enviados no corpo da solicitação
+    data_response = ProcessSearcher(data_request)
+    print(data_request)
+    return data_response.json_response

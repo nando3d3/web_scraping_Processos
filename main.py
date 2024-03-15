a6178ef943269f8ddd27b18f1395389f49fa6f05
Sidney Fernando Ferreira Lemes <sidney092k@gmail.com>
2024-03-15 15:22:49 -0300

    gera exe

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-from fastapi import FastAPI
-
 from fastapi import FastAPI, Request
-import json
+import uvicorn
 from ProcessSearcher import ProcessSearcher
-
 
 app = FastAPI()
 
-
 @app.post("/")
 async def search_process(request: Request):
-
     data_request = (
         await request.json()
-    )  # Obter os dados enviados no corpo da solicitação
+    )
     data_response = ProcessSearcher(data_request)
-    print(data_request)
     return data_response.json_response
+
+if __name__ == "__main__":
+    print("Pressione CTRC + C para fechar...")
+    uvicorn.run(app, host="0.0.0.0", port=8000)

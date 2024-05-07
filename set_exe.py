9dc891dd8b0da414f4a9dce6bb2f04d43720f92e
Sidney Fernando Ferreira Lemes <sidney092k@gmail.com>
2024-05-07 17:55:27 -0300

    att icon

--- set_exe.py
+++ set_exe.py
@@ -4,6 +4,6 @@ import shutil
 shutil.copy2('templates/pesquisa.html', 'dist/templates/pesquisa.html')
 shutil.copy2('templates/resultado.html', 'dist/templates/resultado.html')
 
-subprocess.run(["pyinstaller", "--onefile", "app.py"])
+subprocess.run(["pyinstaller", "--onefile","--icon=search_find.ico", "app.py"])
 
 print("\nexecutável criado com sucesso!")
